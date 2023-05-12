f4cab711f3384728eee7976aba98a63058808d5b
Shinglem <yangmengxin0099@sina.com>
2023-05-12 15:32:45 +0800

    fix jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,25 +8,51 @@ plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     java
     kotlin("jvm")
+    `maven-publish`
 }
 
 group = "com.koql"
 val projectVersion :String by project
 version = projectVersion
 
-dependencies {
-    testImplementation(kotlin("test"))
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-
-
 
 allprojects {
     repositories {
         mavenLocal()
-        maven("https://jitpack.io")
-        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+//        maven("https://maven.aliyun.com/nexus/content/groups/public/")
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://dl.bintray.com/kotlin/exposed")
+        maven("https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://clojars.org/repo/")
     }
 }
+val vertxVersion: String by project
+val jacksonVersion: String by project
+val kotlinCoroutineVersion: String by project
+dependencies {
+
+    //kotlin
+    api(kotlin("stdlib"))
+    api(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = kotlinCoroutineVersion)
+    api(kotlin("reflect"))
+
+    testApi("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testApi("org.junit.jupiter:junit-jupiter-engine")
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.koql"
+            artifactId = "koql"
+
+
+            from(components["java"])
+        }
+    }
+}
+
